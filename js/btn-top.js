90ceab155ad226ed8ffe17dc968c729797063f88
Pongowl <chewie.bricks@gmail.com>
2019-08-07 16:19:32 -0500

    meh

---
+++ js/btn-top.js
@@ -0,0 +1,10 @@
+(function() {
+  "use strict";
+  const btnTop = document.querySelector(".btn-top");
+  btnTop.addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}());
