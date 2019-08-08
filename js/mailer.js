88432257abef004e682cb0c795b5cec9c095c4ad
Pongowl <chewie.bricks@gmail.com>
2019-08-08 16:43:07 -0500

    yo we added a contact me with encryption

---
+++ js/mailer.js
@@ -0,0 +1,10 @@
+
+<SCRIPT TYPE="text/javascript">
+//joe  maller (dot) com credit for this javascript, thank you 
+
+emailE='gmail.com'
+emailE=('pongowl.texas' + '@' + emailE)
+document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
+
+ //-->
+</script>
