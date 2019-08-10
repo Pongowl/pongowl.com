cc332b6ab3a22e6d4029436a46085e44b5c1f71b
Pongowl <chewie.bricks@gmail.com>
2019-08-09 21:08:12 -0500

    jquery

---
+++ js/photos-mobile-scroll.js
@@ -0,0 +1,5 @@
+<script>$(window).scroll(function() {
+  var scrolledY = $(window).scrollTop();
+  $('.photos-header-bg').css('background-position', 'left ' + ((scrolledY)) + 'px');
+    });
+      </script>
